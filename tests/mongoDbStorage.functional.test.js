829b426635f915f09c24f4e477f703ffbccb1e32
Hattan Shobokshi <hattan@gmail.com>
2018-12-21 14:07:50 -0800

    latest updates

--- tests/mongoDbStorage.functional.test.js
+++ tests/mongoDbStorage.functional.test.js
@@ -5,64 +5,44 @@ const {ConversationState} = require('botbuilder');
 
 //require MongoClient to set up fakes and stubs, not actual database connectivity
 const { MongoClient } = require('mongodb');
-
-
-const getSettings = () => ({
-  url: "mongodb://localhost:27017/",
-  database: "botframework",
-  collection: "botframeworkstate"
+const getContext = () => ({
+  turnState : {
+    get : function(){
+      return {};
+    },
+    set : function(){
+      return {};
+    }        
+  },
+  activity : {
+    channelId : "channel1",
+    conversation : {
+      id : "convo"
+    }
+  }
 });
+
 
 describe('MongoDbStorage -Functional', function () {
  
   describe('collection property', async function (done) {
     it('calls MongoClient.db with database name', async function () {
-      //arrange
-      let context = {
-        turnState : {
-          get : function(){
-            return {};
-          },
-          set : function(){
-            return {};
-          }        
-        },
-        activity : {
-          channelId : "channel1",
-          conversation : {
-            id : "convo"
-          }
-        }
-      };
-
+      let context = getContext();
  
       const storage = new MongoDbStorage({
-        url: 'fake_url',
-        database: 'fake_db'
+        url: "mongodb://localhost:27017/",
+        database: "botframework",
+        collection: "functionalteststate"
       });
-      storage.read = function(keys){
-        console.log(keys);
-        return new Promise(function(resolve,reject){
-          resolve({});
-        });
 
-      }
-
+      await storage.connect();
+      
       const conversationState = new ConversationState(storage);
       let property = conversationState.createProperty("foo");
       let state = await property.get(context,{});
       state.foo = "bar";
       property.set(context,state);
-      conversationState.saveChanges(context);
-      
-      
-
- 
-
-
-
-      //assert
-      
+      conversationState.saveChanges(context);     
     });
   });
 
