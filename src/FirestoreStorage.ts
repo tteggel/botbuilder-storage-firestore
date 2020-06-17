d7a0e169407607aa59e3566f368bbe1ab60df3ad
Thom Leggett <thom@bookcreator.com>
2020-06-17 13:20:24 +0100

    Specify top-level collection

--- src/FirestoreStorage.ts
+++ src/FirestoreStorage.ts
@@ -6,16 +6,18 @@ import { Firestore } from '@google-cloud/firestore'
 import { Storage, StoreItems } from 'botbuilder-core'
 
 export class FirestoreStorage implements Storage {
+  readonly collection: string
   readonly settings: object
 
-  public constructor(settings: object = {}) {
+  public constructor(collection, settings: object = {}) {
+    this.collection = collection
     this.settings = settings
   }
 
   public async delete(keys: string[]): Promise<void> {
     const firestore = new Firestore(this.settings)
     try {
-      keys.map(key => firestore.collection('bot').doc(key).delete())
+      keys.map(key => firestore.collection(this.collection).doc(key).delete())
     } finally {
       firestore.terminate()
     }
@@ -24,7 +26,7 @@ export class FirestoreStorage implements Storage {
   public async read(keys: string[]): Promise<StoreItems> {
     const firestore = new Firestore(this.settings)
     try {
-      const refs = keys.map(key => firestore.collection('bot').doc(key))
+      const refs = keys.map(key => firestore.collection(this.collection).doc(key))
       const snapshots = await firestore.getAll(...refs)
 
       const result: StoreItems = {}
@@ -41,7 +43,7 @@ export class FirestoreStorage implements Storage {
     try {
       await Promise.all(
         Object.keys(changes).map(
-          key => firestore.collection('bot')
+          key => firestore.collection(this.collection)
             .doc(key)
             .set(changes[key])
         )
